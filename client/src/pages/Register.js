de1cea5ca00410de834949bd5838f95a1267f086
ruipeng2 <ruipeng2@illinois.com>
2023-11-22 20:27:25 -0800

    Implemented Login and Register in frontends.

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -2,17 +2,35 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    const [email, setEmail] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState(''); // Optional
     const navigate = useNavigate();
-
     const handleSubmit = async (e) => {
         e.preventDefault();
-        // Implement registration logic
-        // On successful registration:
-        localStorage.setItem('token', 'your_token_here'); // Set the token in local storage
-        navigate('/'); // Redirect to the home page
+
+        try {
+            const response = await fetch('http://localhost:4000/api/auth/register', { // Replace with your API URL
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ username, password, email, firstName, lastName, phoneNumber}),
+            });
+
+            const data = await response.json();
+            if (response.ok) {
+                localStorage.setItem('token', data.token); // Save the token
+                navigate('/'); // Redirect to the home page
+            } else {
+                console.error('Registration failed:', data.message); // Handle errors, e.g., show an alert to the user
+            }
+        } catch (error) {
+            console.error('There was a problem with the registration request:', error);
+        }
     };
 
     return (
@@ -21,21 +39,44 @@ const Register = () => {
             <form onSubmit={handleSubmit}>
                 <input
                     type="text"
+                    placeholder="First Name"
+                    value={firstName}
+                    onChange={e => setFirstName(e.target.value)}
+                    required
+                />
+                <input
+                    type="text"
+                    placeholder="Last Name"
+                    value={lastName}
+                    onChange={e => setLastName(e.target.value)}
+                    required
+                />
+                <input
+                    type="text"
                     placeholder="Username"
                     value={username}
                     onChange={e => setUsername(e.target.value)}
+                    required
                 />
                 <input
                     type="email"
                     placeholder="Email"
                     value={email}
                     onChange={e => setEmail(e.target.value)}
+                    required
                 />
                 <input
                     type="password"
                     placeholder="Password"
                     value={password}
                     onChange={e => setPassword(e.target.value)}
+                    required
+                />
+                <input
+                    type="tel"
+                    placeholder="Phone Number (Optional)"
+                    value={phoneNumber}
+                    onChange={e => setPhoneNumber(e.target.value)}
                 />
                 <button type="submit">Register</button>
             </form>
