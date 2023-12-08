361fb52c04de6260bd2ed89b36a7c2caf4ed9ee1
ruipeng2 <ruipeng2@illinois.com>
2023-12-08 16:42:46 -0600

    Resolved NewTask logged out issue.

--- client/src/pages/Newtask.js
+++ client/src/pages/Newtask.js
@@ -1,5 +1,5 @@
 // NewTask.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import NavigationBar from '../components/NavigationBar';
@@ -13,6 +13,34 @@ const NewTask = ({ onTaskAdded }) => {
     { title: 'Home', href: '/' },
     // other links for the profile page
   ];
+
+  useEffect(() => {
+    const checkToken = async () => {
+      const token = localStorage.getItem('token');
+      if (token) {
+        try {
+          const response = await fetch('http://localhost:4000/api/auth/validateToken', {
+            method: 'GET',
+            headers: {
+              'Authorization': token,
+            },
+          });
+
+          if (response.ok) {
+            const data = await response.json();
+            setIsLoggedIn(true);
+            setFirstName(data.user.firstName); // Or however you're storing the first name
+          } else {
+            localStorage.removeItem('token'); // Token is invalid or expired
+            localStorage.removeItem('firstName');
+          }
+        } catch (error) {
+          console.error('Error validating token:', error);
+        }
+      }
+    };
+    checkToken();
+  }, []);
 
   const [task, setTask] = useState({
     username: '', // You may get the username from the logged-in user
