7381d99d7f62c098e25c3e53640997ed40c6295c
ruipeng2 <ruipeng2@illinois.com>
2023-12-10 10:20:14 -0600

    Added a pop up modal for successfully creating a task.

--- client/src/pages/Newtask.js
+++ client/src/pages/Newtask.js
@@ -4,11 +4,12 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import NavigationBar from '../components/NavigationBar';
 const setting = require('../config/config');
-const NewTask = ({ onTaskAdded }) => {
+const NewTask = () => {
   const navigate = useNavigate();
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [firstName, setFirstName] = useState('User');
-
+  const [showSuccessModal, setShowSuccessModal] = useState(false);
+  
   const profileLinks = [
     { title: 'Home', href: '/' },
     { title: "New Task", href: "/new_task" },
@@ -45,13 +46,26 @@ const NewTask = ({ onTaskAdded }) => {
     };
     checkToken();
   }, []);
+  const formatDateToYYYYMMDD = (date) => {
+    const d = new Date(date);
+    let month = '' + (d.getMonth() + 1);
+    let day = '' + d.getDate();
+    let year = d.getFullYear();
 
+    if (month.length < 2) 
+      month = '0' + month;
+    if (day.length < 2) 
+      day = '0' + day;
+
+    return [year, month, day].join('-');
+  };
+  
   const [task, setTask] = useState({
     title: '',
     description: '',
     attachments: [], // Array of objects (fileName, filePath, fileType, fileSize)
     creationDate: '', // Assign the current date on the server side
-    dueDate: null,
+    dueDate: formatDateToYYYYMMDD(new Date()),
     tags: [], // Array of strings
     userDefinedFields: [], // Array of objects (fieldName, fieldValue)
   });
@@ -67,6 +81,11 @@ const NewTask = ({ onTaskAdded }) => {
     setTask((prevTask) => ({ ...prevTask, [name]: value }));
   };
 
+  const handleCancelModal = () => {
+    setShowSuccessModal(false); // Hide the modal
+  };
+
+
   const handleAddTask = async () => {
     const currentDate = new Date();
     task.creationDate = currentDate.toISOString();
@@ -80,51 +99,27 @@ const NewTask = ({ onTaskAdded }) => {
       });
 
       // Assuming the API returns the newly added task, you can update the UI accordingly
-      if (response.data.status === 201) {
-        onTaskAdded(response.data); // Notify the parent component about the new task
+      if (response.status === 201) {
+        // onTaskAdded(response.data); // Notify the parent component about the new task
+        setShowSuccessModal(true); // Show the success modal
         setTask({
           title: '',
           description: '',
           attachments: [],
           creationDate: '',
-          dueDate: null,
+          dueDate: formatDateToYYYYMMDD(new Date()),
           tags: [],
           userDefinedFields: [],
         });
+        setTimeout(() => {
+          setShowSuccessModal(false); // Hide the modal
+          // navigate('/'); // Redirect to home after a delay
+        }, 3000); // 3 seconds delay
       }
     } catch (error) {
       console.error('Error adding task', error);
     }
   };
-
-  useEffect(() => {
-    const checkToken = async () => {
-      const token = localStorage.getItem('token');
-      console.log("token", token)
-      if (token) {
-        try {
-          const response = await fetch(`${setting.base_url}/api/auth/validateToken`, {
-            method: 'GET',
-            headers: {
-              'Authorization': token,
-            },
-          });
-
-          if (response.ok) {
-            const data = await response.json();
-            setIsLoggedIn(true);
-            setFirstName(data.user.firstName); // Or however you're storing the first name
-          } else {
-            localStorage.removeItem('token'); // Token is invalid or expired
-            localStorage.removeItem('firstName');
-          }
-        } catch (error) {
-          console.error('Error validating token:', error);
-        }
-      }
-    };
-    checkToken();
-  }, []);
 
   return (
     // <div className="flex h-screen bg-gray-100">
@@ -193,7 +188,51 @@ const NewTask = ({ onTaskAdded }) => {
           </div>
         </div>
       </div>
+      {showSuccessModal && (
+        <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full" id="my-modal">
+          <div className="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white">
+            <div className="mt-3 text-center">
+              <h3 className="text-lg leading-6 font-medium text-green-700">Task Added Successfully</h3>
+              <div className="mt-2 px-7 py-3">
+                <p className="text-sm text-gray-500">
+                  Your task has been added.
+                </p>
+                <button
+                  type="button"
+                  onClick={handleCancelModal}
+                  className="bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-600 mt-4"
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {/* {showSuccessModal && (
+            <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full" id="my-modal">
+              <div className="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-green-100 text-green-700">
+                <div className="mt-3 text-center">
+                  <h3 className="text-lg leading-6 font-medium">Task Added Successfully</h3>
+                  <div className="mt-2 px-7 py-3">
+                    <p className="text-sm">
+                      Your task has been added.
+                    </p>
+                    <button
+                      type="button"
+                      onClick={handleCancelModal}
+                      className="bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-600 mt-4"
+                    >
+                      Close
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+      )} */}
     </div>
+
   );
   
   
